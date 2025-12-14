d5c63d65f413ec421684812f631cada716f9cb84
abhay-byte <abhay02delhi@gmail.com>
2025-12-14 12:36:09 +0530

    Fix: Android 15/16 compatibility - remove orientation restriction, add 16KB page alignment, update NDK to v29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     namespace = "com.ivarna.finalbenchmark2"
     compileSdk { version = release(36) }
 
-    ndkVersion = "25.2.9519653"
+    ndkVersion = "29.0.13113456"
 
     defaultConfig {
         applicationId = "com.ivarna.finalbenchmark2"
